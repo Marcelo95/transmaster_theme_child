948f7d11ce79da455e3473fe4307d09b33f8dbc4
Marcelo Vieira <marcelovieira1995@gmail.com>
2022-09-27 17:12:26 -0300

    form pt

--- functions.php
+++ functions.php
@@ -153,11 +153,12 @@ function my_form_search($language = "pt-BR")
     ?>
 
     <form style="display: none;" role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+        <div class="wp-block-search__inside-wrapper " style="width: 666px">
         <label>
-            <span class="screen-reader-text"><?php echo _x('Search for:', 'label') ?></span>
-            <input type="search" class="search-field" placeholder="<?php echo esc_attr_x('Search …', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
+            <input type="search" class="wp-block-search__input " placeholder="<?php echo esc_attr_x('Digite aqui …', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
         </label>
-        <input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button') ?>" />
+        <input type="submit" class="wp-block-search__button  " value="Pesquisar" />
+        </div>
     </form>
 
 <?php
